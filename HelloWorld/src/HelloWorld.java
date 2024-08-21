14cf4f07b410cfe6ff78b408694213bb946d324e
Ethan Bull <syntek108@gmail.com>
2024-08-21 16:47:00 -0600

    HelloWorld

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,9 +1,10 @@
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		System.out.println("Hello World!");
-		System.out.println("My name is Ethan :)");
+		System.out.println("Hello");
+		System.out.println("This is very functional :)");
 	}
 
 }
