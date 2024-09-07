2598f0e4433d3b91ed6f47601e0cbcb2379e14c5
Ethan Bull <syntek108@gmail.com>
2024-09-07 13:13:24 -0600

    9/7/24

--- CircleCalculation/src/CircleCalc.java
+++ CircleCalculation/src/CircleCalc.java
@@ -1,8 +1,15 @@
 import java.util.Scanner;
 public class CircleCalc {
 	public static void main(String[] args) {
-		Scanner scn = new
+		Scanner scn = new Scanner(System.in);
 		double radius;
-		
+		System.out.println("Enter the radius of your circle: ");
+		radius = scn.nextDouble();
+		double diameter = radius * 2;
+		double area = Math.pow(radius, 2);
+		double circumference = 2 * Math.PI * radius;
+		System.out.println("Diameter = " + diameter);
+		System.out.println("Area = " + area);
+		System.out.println("Circumference = " + circumference);
 	}
 }
